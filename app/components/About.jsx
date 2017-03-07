35103e5e442c6fb9333572f89911f724b40a0751
Sam Butler-Thompson <sam@q2q-it.com>
2017-03-07 11:50:53 +0000

    Style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
   return (
-    <div>
-      <h3>About</h3>
-      <p>Welcome to the about page!</p>
+    <div className="callout" style={{marginTop: '1rem'}}>
+      <h1>About</h1>
+      <p>This weather apps shows off the React.js application in all its glory, with the ability to connect to an API and get weather information!</p>
+      <p>Here are some of the tools used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react" target="_blank">React</a> - This was the JavaScript framework used
+        </li>
+        <li>
+          <a href="http://openweathermap.org" target="_blank">Open Weather Map</a> - I used Open Weather Map to fetch weather data to the app
+        </li>
+      </ul>
+      <p>Click <Link to="/">here</Link> to get started!</p>
     </div>
   );
 };
